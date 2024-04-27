fea84693f5a76a90c2f971935a4c9500d2a7147b
MrHAM17 <harshminde1@gmail.com>
2024-04-27 20:42:37 +0530

    Its a Trial 02 - Level 01
    25th Mar 2024
    
    Direct from reference code/repo with my code.
    Build on my system.
    
    Working --> Minute issues are there.

---
+++ College Mate/lib/MODULES/Module_CollegeProfile/college_gallary_page.dart
@@ -0,0 +1,186 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:college_mate/UTILS/constants/colors.dart';
+import 'package:college_mate/UTILS/imageview/imageViewer.dart';
+
+class GallaryPage extends StatefulWidget {
+
+   List<String> eventsAndActivities = [
+    'All',
+    'ATRANG',
+    'Events',
+    'Fests',
+    'NSS',
+    'REVIVE',
+    'RESONANCE',
+
+    'AIDS',
+    'Civil',
+    'Computer',
+    'EXTC',
+    'IT',
+    'Mechanic',
+    'Mechatronics',
+  ];
+
+  List<String> years = ['2021', '2022', '2023', '2024'];
+
+  @override
+  _GallaryPageState createState() => _GallaryPageState();
+}
+
+class _GallaryPageState extends State<GallaryPage> {
+  late String selectedEvent;
+  late String selectedYear;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedEvent = widget.eventsAndActivities.first;
+    selectedYear = widget.years.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        primary: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Custom Card
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: customLinearGradient(),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 4),
+              padding: EdgeInsets.all(26),
+              child: Column(
+                children: [
+                  Text(
+                    "Campus Gallary",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          _showEventSelectionDialog();
+                        },
+                        icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                        label: Text('$selectedEvent'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          padding: EdgeInsets.all(10),
+
+                        ),
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          _showYearSelectionDialog();
+                        },
+                        icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                        label: Text('$selectedYear'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          padding: EdgeInsets.all(10),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                        },
+                        child: Icon(Icons.check, color: Colors.black),
+                        style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          primary: Colors.white,
+                          padding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height:1),
+            // Placeholder for fetching data from Firebase
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showEventSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Select Event'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: widget.eventsAndActivities.map((event) {
+                return ListTile(
+                  title: Text(event),
+                  onTap: () {
+                    setState(() {
+                      selectedEvent = event;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  _showYearSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Select Year'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: widget.years.map((year) {
+                return ListTile(
+                  title: Text(year),
+                  onTap: () {
+                    setState(() {
+                      selectedYear = year;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  LinearGradient customLinearGradient() {
+    return const LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.topRight,
+      colors: [
+        Color(0xff43cea2),
+        Color(0xff185a9d),
+      ],
+    );
+  }
+}
---
+++ College Mate/lib/MODULES/Module_T&P/campus_placement_screen.dart
@@ -0,0 +1,213 @@
+import 'package:college_mate/MODULES/Module_T&P/study_material_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:college_mate/UTILS/constants/colors.dart';
+import 'package:college_mate/UTILS/imageview/imageViewer.dart';
+
+class CampusPlacementScreen extends StatefulWidget {
+  String role;
+  CampusPlacementScreen({Key? key, required this.role}) : super(key: key);
+
+  @override
+  _CampusPlacementScreenState createState() => _CampusPlacementScreenState();
+}
+
+class _CampusPlacementScreenState extends State<CampusPlacementScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(122),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: customLinearGradient(),
+            // borderRadius: BorderRadius.only(
+            //   bottomLeft: Radius.circular(20),
+            //   bottomRight: Radius.circular(20),
+            // ),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: AppBar(
+            elevation: 10,
+            backgroundColor: Colors.transparent,
+            title: Text(
+              'Campus Placement',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            centerTitle: true,
+            bottom: TabBar(
+              controller: _tabController,
+              indicator: BoxDecoration(
+                // gradient: customLinearGradient(),
+                color: Colors.white30,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.white,
+              tabs: [
+                Tab(text: 'Updates'),
+                Tab(text: 'My App.'),
+                Tab(text: 'My Toolkit'),
+              ],
+              padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),  // Added space from left and right
+        child: TabBarView(
+          controller: _tabController,
+          children: [
+            _buildUpdatesNoticesTab(),
+            _buildAppliedOpportunitiesTab(),
+            _buildToolkitTab(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUpdatesNoticesTab() {
+    return ListView.builder(
+      itemCount: 5, // Replace with actual data count
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          child: ListTile(
+            title: Text('Update ${index + 1}'),
+            subtitle: Text('Details about update ${index + 1}'),
+            onTap: () {
+              // Navigate to update details page or display update
+            },
+          ),
+        );
+      },
+      padding: EdgeInsets.fromLTRB(0,6, 0, 0),
+    );
+  }
+
+  Widget _buildAppliedOpportunitiesTab() {
+    return ListView.builder(
+      itemCount: 3, // Replace with actual data count
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          child: ListTile(
+            title: Text('Applied Opportunity ${index + 1}'),
+            subtitle: Text('Details about applied opportunity ${index + 1}'),
+            onTap: () {
+              // Navigate to applied opportunity details page
+            },
+          ),
+        );
+      },
+      padding: EdgeInsets.fromLTRB(0,6, 0, 0),
+    );
+  }
+
+
+
+  Widget _buildToolkitTab() {
+    return GridView.count(
+      padding: EdgeInsets.fromLTRB(0,6, 0, 0),
+      crossAxisCount: 2,
+      childAspectRatio: 1.0,
+      mainAxisSpacing: 15.0,
+      crossAxisSpacing: 15.0,
+      children: [
+        _buildCubeCard(
+          title: 'Search',
+          icon: Icons.search,
+          onTap: () {
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(
+            //     builder: (context) => YourSearchPage(), // Replace 'YourSearchPage' with the name of your search page
+            //   ),
+            // );
+          },
+        ),
+        _buildCubeCard(
+          title: 'Apply',
+          icon: Icons.check_circle,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => StudyMaterialScreen(), // Replace 'YourSearchPage' with the name of your search page
+              ),
+            );          },
+        ),
+        _buildCubeCard(
+          title: 'Progress',
+          icon: Icons.show_chart,
+          onTap: () {
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(
+            //     builder: (context) => YourSearchPage(), // Replace 'YourSearchPage' with the name of your search page
+            //   ),
+            // );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCubeCard({required String title, required IconData icon, required VoidCallback onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 50.0,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  LinearGradient customLinearGradient() {
+    return const LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.topRight,
+      colors: [
+        Color(0xff43cea2),
+        Color(0xff185a9d),
+      ],
+    );
+  }
+}
